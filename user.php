294a925cb2f8c274f60645b79279eac699018a11
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-05-31 01:46:16 +0000

    Added checkUsername() and processLogin() functions

--- user.php
+++ user.php
@@ -13,11 +13,64 @@ Class User {
 
     public function create_user($username, $password) {
         $db = db_connect();
-        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "
+            INSERT INTO users (username, password)
+            VALUES (:username, :password)
+        ";
         $stmt = $db->prepare($sql);
-        $stmt->bindParam(':username', $username);
-        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->bindParam(':password', $hash,     PDO::PARAM_STR);
         $stmt->execute();
-        return $rows;
+        return $db->lastInsertId(); // If we need to know the ID of the new user
+    }
+
+    public function checkUsername($username)
+    {
+        $db = db_connect();
+        $sql = "
+            SELECT id
+              FROM accounts
+             WHERE username = :username
+             LIMIT 1
+        ";
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+        $stmt->execute();
+        $userAccount = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt->closeCursor();
+    
+        // If fetch() returned false or null, that username does not exist
+        return ($userAccount === false || $userAccount === null);
+    }
+
+    public function processLogin($username, $password)
+    {
+        $db = db_connect();
+    
+        $sql = "
+            SELECT *
+              FROM accounts
+             WHERE username = :username
+             LIMIT 1
+        ";
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+        $stmt->execute();
+        $userAccount = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt->closeCursor();
+    
+        // At this point, $useraccount is either empty or has a value
+        if ($userAccount !== false && $userAccount !== null) {
+            // Compare plaintext $password against the stored hash
+            if (password_verify($password, $userAccount['password'])) {
+                return $userAccount;
+            } else {
+                // Wrong password
+                return null;
+            }
+        }    
+        // Username not found
+        return null;
     }
 }
