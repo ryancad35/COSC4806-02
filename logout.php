69a592abf1370efa1d556ab8ddf9ace52f0991d1
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-05-31 01:03:16 +0000

    Added logout page that kills / destroys session

---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+session_destroy();
+header('Location: login.php');
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Logout</title>
+</head>
+
+<body>
+    <h1>Assignment #2</h1>
+</body>
+
+</html>
