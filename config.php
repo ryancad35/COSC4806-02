4e3d6125ba057899db59e11b695e89b7b3ad58bb
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-05-31 00:39:24 +0000

    Remove config.php from tracking, add to .gitignore

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', 'p880q.h.filess.io');
-define('DB_USER', 'cosc4806_solarspace');
-define('DB_PASS', $_ENV['DB_PASS']);
-define('DB_DATABASE', 'cosc4806_solarspace');
-define('DB_PORT', '3305');
-?>
