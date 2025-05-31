78ae78b917e2ea5812a9dd65ad36b611e1ca0743
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-05-31 02:24:59 +0000

    Added logic for registering an account
    - Check if fields are empty
    - Check if password > 10 chars
    - Username check
    - Redirect to loginSuccess.php once account is created

---
+++ loginSuccess.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login Success</title>
+    </head>
+    <body>
+    <main>
+        <h1>Login Success!</h1>
+        <p>Your account has been created.</p>
+        <p><a href="login.php">Click here to login</a></p>
+    </main>
+    </body>
+</html>
--- register.php
+++ register.php
@@ -1,6 +1,51 @@
 <?php 
+session_start();
 require_once('user.php');
-require_once('database.php');
+
+// Redirect to index.php if user is already logged in
+if (!empty($_SESSION['authenticated'])) {
+    header('Location: index.php');
+    exit();
+}
+
+$error    = '';
+$username = '';
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action === null) {
+    $action = filter_input(INPUT_GET, 'action');
+}
+
+// Check if user has clicked on the submit button
+if ($action === 'Submit Registration') {
+    $username = trim(filter_input(INPUT_POST, 'username'));
+    $password = trim(filter_input(INPUT_POST, 'password'));
+
+    // Check if fields are empty
+    if (!notEmptyAccount($username, $password)) {
+        $error = 'Username and password cannot be empty.';
+    }
+    // Check if password is at least 10 characters long
+    elseif (strlen($password) < 10) {
+        $error = 'Password must be at least 10 characters long.';
+    }
+    // At this point, both fields are not empty and password is at least 10 characters long
+    else {
+        $userObj = new User();
+
+        // Check if username already exists
+        if (! $userObj->checkUsername($username)) {
+            $error = 'That username is already taken. Please choose another.';
+        }
+        else {
+            // Username does not exist, create new user
+            $newUserId = $userObj->create_user($username, $password);
+            if ($newUserId) {
+                header('Location: loginSuccess.php');
+            }
+        }  
+    }
+}
 
 ?>
 
@@ -20,7 +65,8 @@ require_once('database.php');
                 <input type="text" name="username" value="<?php echo $username ?>">
                 <br><br>
                 <label>Password:</label>
-                <input type="text" name="password"><br>
+                <input type="text" name="password">
+                <br><br>
                 <input type="submit" name ="action" value="Submit Registration"><br>
                 <p>Already have an account? <a href="login.php">Login here</a></p>
         </form>
