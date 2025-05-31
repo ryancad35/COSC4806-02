5a99a70e1b195af0dcb1da73b6d4d9e739e598b4
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-05-31 02:32:37 +0000

    Added validation logic

---
+++ validate.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+require_once 'user.php'; 
+
+$username = trim($_POST['username']  ?? '');
+$password = trim($_POST['password']  ?? '');
+
+// Treat it as failed login attempt if either field is empty
+if (notEmptyAccount($username, $password) == false) {
+    // Count failed attempts
+    if (isset($_SESSION['failedAttempts'])) {
+        $_SESSION['failedAttempts']++;
+    } else {
+        $_SESSION['failedAttempts'] = 1;
+    }
+    header('Location: login.php');
+    exit;
+}
+
+// Verify username and password
+$userObj = new User();
+$user = $userObj->processLogin($username, $password);
+
+if ($user !== null) {
+    // Login successful, perform the following steps:
+
+    $_SESSION['isAuthenticated'] = true;
+    $_SESSION['username'] = $user['username'];
+
+    if (isset($_SESSION['loginSuccess'])) {
+        $_SESSION['loginSuccess']++;
+    } else {
+        $_SESSION['loginSuccess'] = 1;
+    }
+    header('Location: index.php');
+    exit;
+} else {
+    // Login failed, track failed attempts
+    if (isset($_SESSION['failedAttempts'])) {
+        $_SESSION['failedAttempts']++;
+    } else {
+        $_SESSION['failedAttempts'] = 1;
+    }
+    header('Location: login.php');
+    exit;
+}
